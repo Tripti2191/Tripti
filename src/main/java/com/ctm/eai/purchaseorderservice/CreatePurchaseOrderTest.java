8cdcd78702d08235a7941e010ae63fd1ec69d897
Praveen-Bhasker <praveen.bhasker@officedepot.com>
2017-06-16 10:59:31 -0400

    Added more code for verification

--- src/main/java/com/ctm/eai/purchaseorderservice/CreatePurchaseOrderTest.java
+++ src/main/java/com/ctm/eai/purchaseorderservice/CreatePurchaseOrderTest.java
@@ -1,14 +1,16 @@
 package com.ctm.eai.purchaseorderservice;
 
+import java.io.IOException;
+
+import org.apache.velocity.VelocityContext;
 import org.testng.annotations.Test;
 
-import com.ctm.common.utils.Utilities;
 import com.ctm.core.CoreLibraries;
 import com.ctm.services.annotation.ServiceDataFile;
-import com.ctm.services.annotation.ServiceRequestFile;
 import com.ctm.services.dataproviders.ServicesDataProvider;
 import com.ctm.services.xml.CommonServiceVerificationLibraries;
 import com.ctm.services.xml.CtmServicesHandler;
+import com.ctm.services.xml.CtmXmlServiceLibraries;
 import com.ctm.services.xml.ServiceAttributesContainer;
 import com.ctm.services.xml.ServicePropertiesContainer;
 
@@ -24,26 +26,51 @@ public class CreatePurchaseOrderTest extends CoreLibraries {
 
 	@Test(dataProviderClass = ServicesDataProvider.class, dataProvider = "Service_DataFeed_Provider")
 	@ServiceDataFile("ServiceData/CreatePurchaseOrder/CreatePurchaseOrder.txt")
-	@ServiceRequestFile("ServiceData/CreatePurchaseOrder/CreatePurchaseOrderRequest.xml")
-	public void testCreatePurchaseOrder() {
+	public void testCreatePurchaseOrder(String index, String consumerName, String consumerTransactionID,
+			String requestID, String orderDate, String orderType, String vendorID, String totalLines,
+			String totalQuantities, String expectedResult) throws IOException {
+
 		//Instantiation Part 
 		ServicePropertiesContainer propertiesContainer = new ServicePropertiesContainer();
 		CtmServicesHandler xmlServiceHandler = new CtmServicesHandler();
-		CommonServiceVerificationLibraries lib = new CommonServiceVerificationLibraries();
-		Utilities utils = new Utilities();
+		CommonServiceVerificationLibraries serviceVerificationLibraries = new CommonServiceVerificationLibraries();
+		CtmXmlServiceLibraries xmlServiceLibrary = new CtmXmlServiceLibraries();
 
-		//Set properties required to post the payload and get response
+		//Set properties required to post the payload and get response (setting 4 properties are mandatory. setUserName, setPassword, setIsSoap, setBodyOrEnvelope)
 		propertiesContainer.setUserName(getData("testData.serviceUserName"));
 		propertiesContainer.setPassword(getData("testData.servicePassword"));
 		propertiesContainer.setIsSoap(true);
-		String envelope = utils.readFile("/Users/praveen-bhasker/Desktop/ODP/EAI/PurchaseOrderService/envelope.xml");
-		propertiesContainer.setBodyOrEnvelope(envelope);
+		String body = xmlServiceLibrary
+				.getRequestBodyFromFile("ServiceData/CreatePurchaseOrder/CreatePurchaseOrderRequest.xml");
+		String templateReplacedBody = xmlServiceLibrary.replaceTemplateWithValues(body,
+				createContextForReplacement(consumerName, consumerTransactionID, requestID, orderDate, orderType,
+						vendorID, totalLines, totalQuantities));
+		propertiesContainer.setBodyOrEnvelope(templateReplacedBody);
 
+		//Build service container and get response
 		xmlServiceHandler.buildServiceContainer(propertiesContainer);
 		ServiceAttributesContainer container = xmlServiceHandler.getServiceAttributesContainer();
 		Response response = container.getResponse();
-		lib.verifyStatusCode(response, 200);
+
+		//Validation part
+		serviceVerificationLibraries.verifyStatusCode(response, 200);
+
 		response.prettyPrint();
+	}
+
+	private VelocityContext createContextForReplacement(String consumerName, String consumerTransactionID,
+			String requestID, String orderDate, String orderType, String vendorID, String totalLines,
+			String totalQuantities) {
+		VelocityContext context = new VelocityContext();
+		context.put("CONSUMER_NAME", consumerName);
+		context.put("CONSUMER_TRANSACTION_ID", consumerTransactionID);
+		context.put("REQUEST_ID", requestID);
+		context.put("ORDER_DATE", orderDate);
+		context.put("ORDER_TYPE", orderType);
+		context.put("VENDOR_ID", vendorID);
+		context.put("TOTAL_LINES", totalLines);
+		context.put("TOTAL_QUANTITIES", totalQuantities);
+		return context;
 	}
 
 }
