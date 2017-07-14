a6a1bc3292a2bdbbf94484d7752b766428a629b0
tejasvee-saini <tejasvee.saini@officedepot.com>
2017-07-14 16:24:52 -0400

    created skeleton for bizclub

--- src/main/java/com/ctm/eai/BizClub/BizClub_Libs.java
+++ src/main/java/com/ctm/eai/BizClub/BizClub_Libs.java
@@ -1,9 +1,29 @@
 package com.ctm.eai.BizClub;
 
+import java.util.HashMap;
+
+import org.apache.velocity.VelocityContext;
+
 import com.ctm.core.CoreLibraries;
 import com.ctm.logger.ConsoleReport;
 
 public class BizClub_Libs extends CoreLibraries{
 		public ConsoleReport consoleReport = new ConsoleReport();
+		
+		public VelocityContext createContextForReplacement(String[] attributes,String[] attrValues) {
+			VelocityContext context = new VelocityContext();
+
+			if(attributes.length!=(attrValues.length))
+				System.out.println("Error :"+attributes.length+" not equal to "+(attrValues.length));
+			else
+				System.out.println("Match :"+attributes.length+" equal to "+(attrValues.length));
+			
+			for(int i=0;i<attributes.length;i++)
+				context.put(attributes[i], attrValues[i]);
+			
+			return context;
+		}
+		
+		
 		
 }
--- src/main/java/com/ctm/eai/BizClub/CustomerOrderCreditAuthorization.java
+++ src/main/java/com/ctm/eai/BizClub/CustomerOrderCreditAuthorization.java
@@ -1,6 +1,5 @@
 package com.ctm.eai.BizClub;
 
-import org.apache.velocity.VelocityContext;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -22,8 +21,8 @@ import io.restassured.response.Response;
 public class CustomerOrderCreditAuthorization extends BizClub_Libs{
 	
 	@Test(dataProviderClass = ServicesDataProvider.class, dataProvider = "Service_DataFeed_Provider")
-	@ServiceDataFile("ServiceData/BizBox/CustomerOrderCreditAuthorization.txt")
-	public void testCustomerOrderCreditAuthorization(String index, String schoolID){
+	@ServiceDataFile("ServiceData/BizClub/CustomerOrderCreditAuthorization.txt")
+	public void testCustomerOrderCreditAuthorization(String index,String expectedResult,String consumerName,String consumerTransactionId,String consumerTransactionDateTime,String firstName,String middleName,String lastName,String cardHighValueToken,String expirationDate,String cardType,String amount,String applicationTransactionNumber,String name,String bill_address1,String bill_address2,String bill_city,String bill_state,String bill_postalCode,String bill_country,String orderType,String brandType,String orderSource,String customerOrderNumber,String originalOrderNumber,String orderDate,String acc_costCenter,String acc_costCenterDesc,String poNumber,String acc_release,String serialNumber,String subTotal,String totals_tax,String delivery,String totals_discount,String misc,String total,String lineNumber,String sku,String odSku,String customerSku,String vendorProductCode,String customProductCode,String quantity,String unitPrice,String item_tax,String item_discount,String cost,String unitOfMeasure,String description,String comments,String item_costCenter,String item_costCenterDesc,String costCenterSeq,String costCenterRoute,String item_release,String configID,String bundleID,String invLoc,String InventoryLocation,String id,String seq,String Name,String ship_address1,String ship_address2,String address3,String ship_city,String ship_state,String ship_postalCode,String ship_country,String email,String phoneNumber,String faxNumber){
 		//Instantiation Part 
 		ServicePropertiesContainer propertiesContainer = new ServicePropertiesContainer();
 		ServicesHandler objServiceHandler = new ServicesHandler();
@@ -32,22 +31,18 @@ public class CustomerOrderCreditAuthorization extends BizClub_Libs{
 		//Set properties required to post the payload and get response
 		propertiesContainer.setIsRest(true);
 		propertiesContainer.setIsGetOrPost("Post");
-		String body = objCommonServiceLibraries.getRequestBodyFromFile("ServiceData/BizBox/CustomerOrderCreditAuthorization.json");
-		String templateReplacedBody = objCommonServiceLibraries.replaceTemplateWithValues(body,createContextForReplacement(schoolID));
+		String body = objCommonServiceLibraries.getRequestBodyFromFile("ServiceData/BizClub/CustomerOrderCreditAuthorization.json");
+		String[] attrNames={"consumerName","consumerTransactionId","consumerTransactionDateTime","firstName","middleName","lastName","cardHighValueToken","expirationDate","cardType","amount","applicationTransactionNumber","name","bill_address1","bill_address2","bill_city","bill_state","bill_postalCode","bill_country","orderType","brandType","orderSource","customerOrderNumber","originalOrderNumber","orderDate","acc_costCenter","acc_costCenterDesc","poNumber","acc_release","serialNumber","subTotal","totals_tax","delivery","totals_discount","misc","total","lineNumber","sku","odSku","customerSku","vendorProductCode","customProductCode","quantity","unitPrice","item_tax","item_discount","cost","unitOfMeasure","description","comments","item_costCenter","item_costCenterDesc","costCenterSeq","costCenterRoute","item_release","configID","bundleID","invLoc","InventoryLocation","id","seq","Name","ship_address1","ship_address2","address3","ship_city","ship_state","ship_postalCode","ship_country","email","phoneNumber","faxNumber"};
+		String[] attrValues={consumerName,consumerTransactionId,consumerTransactionDateTime,firstName,middleName,lastName,cardHighValueToken,expirationDate,cardType,amount,applicationTransactionNumber,name,bill_address1,bill_address2,bill_city,bill_state,bill_postalCode,bill_country,orderType,brandType,orderSource,customerOrderNumber,originalOrderNumber,orderDate,acc_costCenter,acc_costCenterDesc,poNumber,acc_release,serialNumber,subTotal,totals_tax,delivery,totals_discount,misc,total,lineNumber,sku,odSku,customerSku,vendorProductCode,customProductCode,quantity,unitPrice,item_tax,item_discount,cost,unitOfMeasure,description,comments,item_costCenter,item_costCenterDesc,costCenterSeq,costCenterRoute,item_release,configID,bundleID,invLoc,InventoryLocation,id,seq,Name,ship_address1,ship_address2,address3,ship_city,ship_state,ship_postalCode,ship_country,email,phoneNumber,faxNumber};
+		String templateReplacedBody = objCommonServiceLibraries.replaceTemplateWithValues(body,createContextForReplacement(attrNames,attrValues));
 		propertiesContainer.setBodyOrEnvelope(templateReplacedBody);
-		
+		System.out.println(templateReplacedBody.toString());
 		//Build service container and get response
 		objServiceHandler.buildServiceContainer(propertiesContainer);
 		ServiceAttributesContainer container = objServiceHandler.getServiceAttributesContainer();
 		Response response = container.getResponse();
 		response.prettyPrint();
-		
-		
 	}
 	
-	private VelocityContext createContextForReplacement(String schoolID) {
-		VelocityContext context = new VelocityContext();
-		context.put("schoolID", schoolID);
-		return context;
-	}
+	
 }
